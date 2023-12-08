4ce55603ed52157d5b5de42c1f6979e71ba9697c
laughterhao <laughterhao@gmail.com>
2023-12-08 10:18:24 +0800

    pdo

--- DB_conn.php
+++ DB_conn.php
@@ -1,3 +1,15 @@
 <?php
+$host ="localhost";
+$user = "Nic";
+$passeword = "123456";
+$dbname = "diving";
 
+try{
+  $conn = new PDO("mysql:host=$host;dbname=$dbname",$user,$passeword);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "連接成功";
+}catch(PDOException $e){
+  echo "連接失敗".$e->getMessage();
+}
 ?>
--- add_new.php
+++ add_new.php
@@ -1,22 +1,18 @@
 <?php
-$host ="localhost";
-$user = "Nic";
-$passeword = "123456";
-$dbname = "diving";
+require_once("DB_conn.php");
+$title_img = $_POST["title_img"];
+$title = $_POST["title"];
+$sort = $_POST["sort"];
+$content = $_POST["content"];
+$img = $_POST["img"];
+$end_date = $_POST["end_date"];
+$timestamp = date("Y-m-d");
 
-try{
-  $conn = new PDO("mysql:host = $host;dbname = $dbname",$user,$passeword);
-
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "連接成功";
-}catch(PDOException $e){
-  echo "連接失敗".$e->getMessage();
+try {
+  $sql = "INSERT INTO `new` (`Sub_img`, `Title`, `Sort`, `Content`, `Main_img`, `Exp_date`, `Create_at`) VALUES (:title_img, :title, :sort, :content, :img, :end_date, :Create_at)";
+  $statement = $conn->prepare($sql); //資料先作暫存
+  $statement->execute([':title_img' => $title_img, ':title' => $title, ':sort' => $sort, ':content' => $content, ':img' => $img, ':end_date' => $end_date, ':Create_at' => $timestamp]);
+} catch (PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
 }
-
-$sql = "INSERT INTO new (title, Content,)
-VALUES ('Cardinal', 'Tom B. Erichsen')";
-
-$RS=$conn->query($sql);
-if($RS->rowCount() > 0) {echo "新增紀錄成功";}
-else {echo "新增紀錄失敗";}
-?>
+$conn = null;
